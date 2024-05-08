804f449f6bf6b4aeb7ae85cbadd13cddb9a1730e
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-05-08 21:41:31 +0100

    Fix checker requirement #2

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-""" Task 1. Module """
+""" Task 1. Module
+"""
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """Comment """
+    """ Comment
+    """
     def __init__(self):
-        """Initialize derived class with the parent class init method
+        """ Initialize derived class with the parent class init method
         """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        """Assign to the dictionary self.cache_data the item
-        value for the key `key`
+        """ Assign to the dictionary self.cache_data the item
+            value for the key `key`
         """
         if key is None or item is None:
             pass
@@ -27,8 +29,8 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Returns the value in self.cache_data linked to key
-        otherwise, None is returned
+        """ Returns the value in self.cache_data linked to key
+            otherwise, None is returned
         """
         if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
